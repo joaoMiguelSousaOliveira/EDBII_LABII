673713accfc7ea0f1ca4af277b47de1875232f48
João <jjoaomiguel05@gmail.com>
2025-10-23 23:23:06 -0300

    Implementa as funcionalidades de  main.py. Introduz uma classe TerminalUI para gerenciar cores, cabeçalhos e limpeza de tela. A visualização das árvores foi implementada  com um layout e colorido.    * Arquivos afetados: main.py, src/

--- main.py
+++ main.py
@@ -165,4 +165,4 @@ if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
-        print("\n\nOperação interrompida pelo usuário. Saindo")
+        print("\n\nOperação interrompida pelo usuário. Saindo...")
